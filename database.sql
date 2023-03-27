b65f12ded83a24b0828817ec777a6b2df405c0ad
Haemaks <Martin_Petkov15@outlook.com>
2023-03-27 14:33:09 +0300

    Added empty login servlet and set up the database file

--- database.sql
+++ database.sql
@@ -1,2 +1,4 @@
-CREATE DATABASE users;
-USE users;
+CREATE DATABASE website_users;
+USE website_users;
+CREATE TABLE personal_information (egn INT, name VARCHAR(255));
+INSERT INTO personal_information VALUES (1, "Peter");
